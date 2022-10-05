9a59b88e4a4e39bf63903a39a7230f6bdd559dd2
Oleksii Honchar <alekseyhonchar@gmail.com>
2022-10-05 17:17:53 +0100

    refactor: fixed route

--- services/comments/src/routes/Comment.Router.js
+++ services/comments/src/routes/Comment.Router.js
@@ -3,8 +3,8 @@ import CommentController from "../controllers/Comment.Controller.js";
 const router = Router();
 
 router.get('/api/posts/:id/comments', CommentController.getAllComments);
-router.post('/api/posts/:id/', CommentController.findCommentById);
-router.post('/api/posts/:id/new-comment', CommentController.addComment);
+router.get('/api/posts/:id/comments/:id/', CommentController.findCommentById);
+router.post('/api/posts/:id/comments', CommentController.addComment);
 router.delete('/api/posts/:id/comments/:id', CommentController.deleteCommentById);
 
 export default router;
