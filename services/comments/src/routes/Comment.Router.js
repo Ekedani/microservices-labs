0ca67cfae7624035b3869c05a11676e8bfd418fc
Oleksii Honchar <alekseyhonchar@gmail.com>
2022-10-05 13:13:27 +0100

    refactor: changed routes

--- services/comments/src/routes/Comment.Router.js
+++ services/comments/src/routes/Comment.Router.js
@@ -2,9 +2,9 @@ import { Router } from 'express';
 import CommentController from "../controllers/Comment.Controller.js";
 const router = Router();
 
-router.get('/posts/:id/comments', CommentController.getAllComments);
-router.post('/posts/:id/', CommentController.findCommentById);
-router.post('/posts/:id/new-comment', CommentController.addComment);
-router.delete('/posts/:id/comments/:id', CommentController.deleteCommentById);
+router.get('/api/posts/:id/comments', CommentController.getAllComments);
+router.post('/api/posts/:id/', CommentController.findCommentById);
+router.post('/api/posts/:id/new-comment', CommentController.addComment);
+router.delete('/api/posts/:id/comments/:id', CommentController.deleteCommentById);
 
 export default router;
