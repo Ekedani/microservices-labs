12bbcc9bd3623ae166e571d1a24c89475462f8a4
Alexey Bilko <bilkoalexey67@gmail.com>
2022-10-06 20:01:01 +0300

    fixed

--- services/posts/PostService/Program.cs
+++ services/posts/PostService/Program.cs
@@ -1,16 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Net;
 
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 }
-
 app.UseCors(x => x
 .AllowAnyOrigin()
 .AllowAnyMethod()
