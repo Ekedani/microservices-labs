357462599035cd2a8ebd8a40ed88971a0bf04c42
Oleksii Honchar <alekseyhonchar@gmail.com>
2022-10-06 11:12:53 +0100

    feat: added author to post modal

--- services/client/src/components/common/post-modal/index.jsx
+++ services/client/src/components/common/post-modal/index.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './post-modal.module.scss';
 
-export const PostModal = ({header, body, onClick}) => {
+export const PostModal = ({header, body, authorID, onClick}) => {
   const WORDS_PER_MINUTE = 200;
   //TODO: get from props
-  let data = {
+  const [data, setData] = useState({
     header,
-    body: 'Body text',
-  };
+    body,
+  });
+  useEffect(() => {
+    if (authorID) {
+      const author = {
+        username: 'prikol',
+        tag: 'mem',
+      }
+      setData({...data, author: author});
+    }
+  }, []);
   const numberOfWords = data.body.split(' ').length;
   const timeToRead = Math.ceil(numberOfWords / WORDS_PER_MINUTE);
   return (
       <div className={styles.container} onClick={onClick}>
-        <header>{header}</header>
+        <div>
+          <header>{header}</header>
+          {
+            data.author ?
+            <p>&nbsp;by&nbsp;<a href={'google.com'}>{data.author.username}</a></p>
+            :
+            <></>
+          }
+        </div>
         <span>🕐 {timeToRead}min</span>
       </div>
     )
