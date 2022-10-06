c85594709ecb0b30927c86f7bf874145c64b50f4
Oleksii Honchar <alekseyhonchar@gmail.com>
2022-10-06 14:11:29 +0100

    feat: added routes

--- services/client/src/App.js
+++ services/client/src/App.js
@@ -10,6 +10,9 @@ import './App.css';
 import { NavBar } from "./components/basic/navbar";
 import { HomePage } from './components/pages/homepage';
 import { UsersPage } from "./components/pages/userspage";
+import { User } from "./components/pages/user";
+import { ErrorPage } from "./components/pages/errorpage";
+import { Post } from "./components/pages/post";
 function App() {
   return (
     <Router>
@@ -17,7 +20,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/api/posts" />} />
         <Route path="/api/posts" element={<HomePage />} />
+        <Route path="/api/posts/:id" element={<Post />} />
         <Route path="/api/users" element={<UsersPage />} />
+        <Route path="/api/users/:id" element={<User />} />
+        <Route path="/oops" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
