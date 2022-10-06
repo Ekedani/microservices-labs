2789cc03e6ea0965b83946b11aabfca9265f0685
Oleksii Honchar <alekseyhonchar@gmail.com>
2022-10-06 12:51:12 +0100

    fix: fixed margin

--- services/client/src/App.js
+++ services/client/src/App.js
@@ -7,11 +7,13 @@ import {
 } from "react-router-dom";
 
 import './App.css';
+import { NavBar } from "./components/basic/navbar";
 import { HomePage } from './components/pages/homepage';
 import { UsersPage } from "./components/pages/userspage";
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Navigate to="/feed" />} />
         <Route path="/feed" element={<HomePage />} />
