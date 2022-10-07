71774b446e0a80d3e4770c33320edac57c31415c
Vladlen <ilusenkovladlen@gmail.com>
2022-10-07 09:42:23 +0300

    Another changes

--- services/comments/src/app.js
+++ services/comments/src/app.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/comments', commentRouter);
+app.use('/api/posts/:postID/comments', commentRouter);
 
 export default app;
--- services/comments/src/routes/Comment.Router.js
+++ services/comments/src/routes/Comment.Router.js
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get('/api/posts/:id/comments', CommentController.getAllComments);
 router.get('/api/posts/:id/comments/:id/', CommentController.findCommentById);
-router.post('/api/posts/:id/comments', CommentController.addComment);
+router.post('/api/posts/:id/comments', CommentController.validate(), CommentController.addComment);
 router.delete('/api/posts/:id/comments/:id', CommentController.deleteCommentById);
 
 export default router;
