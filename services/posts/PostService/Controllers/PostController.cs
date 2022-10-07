ca2ea0428114047e33e4d77fc3b6bc46d43f3248
Alexey Bilko <bilkoalexey67@gmail.com>
2022-10-07 13:15:57 +0300

    hardcoded data

--- services/posts/PostService/Controllers/PostController.cs
+++ services/posts/PostService/Controllers/PostController.cs
@@ -10,12 +10,19 @@ namespace PostService.Controllers
     [Route("api/posts")]
     public class PostController : ControllerBase
     {
-        private List<Post> Posts = new List<Post>();
+        private List<Post> Posts = new List<Post>() 
+        { 
+            new Post() { Id = 1, Header = "Header 1", Body = "Some Text in Post 1", Author_Id = "1", Tag = "tag", Username = "username1" }, 
+            new Post() { Id = 2, Header = "Header 2", Body = "Some Text in Post 2", Author_Id = "1", Tag = "tag", Username = "username1" }, 
+            new Post() { Id = 3, Header = "Header 3", Body = "Some Text in Post 3", Author_Id = "1", Tag = "tag", Username = "username1" }, 
+            new Post() { Id = 4, Header = "Header 4", Body = "Some Text in Post 4", Author_Id = "1", Tag = "tag", Username = "username1" }, 
+            new Post() { Id = 5, Header = "Header 5", Body = "Some Text in Post 5", Author_Id = "1", Tag = "tag", Username = "username1" } 
+        };
 
         public PostController()
         {
-            var jsonText = System.IO.File.ReadAllText(@"data.txt");
-            Posts = (List<Post>)JsonConvert.DeserializeObject<IList<Post>>(jsonText);
+            //var jsonText = System.IO.File.ReadAllText(@"data.txt");
+            //Posts = (List<Post>)JsonConvert.DeserializeObject<IList<Post>>(jsonText);
         }
 
         [HttpGet]
@@ -36,12 +43,17 @@ namespace PostService.Controllers
         [Route("Add")]
         public async Task<ActionResult<Post>> Add(Post post)
         {
-            string json = JsonConvert.SerializeObject(post);
-            if (json != null)
+            if (post != null)
             {
-                System.IO.File.WriteAllText(@"data.txt", json);
+                Posts.Add(post);
                 return Ok(post);
             }
+            //string json = JsonConvert.SerializeObject(post);
+            //if (json != null)
+            //{
+            //    System.IO.File.WriteAllText(@"data.txt", json);
+            //    return Ok(post);
+            //}
             return BadRequest();
         }
 
@@ -51,8 +63,8 @@ namespace PostService.Controllers
         {
             if (Posts.Remove(Posts.First(x => x.Id == id)))
             {
-                string json = JsonConvert.SerializeObject(Posts);
-                System.IO.File.WriteAllText(@"data.txt", json);
+                //string json = JsonConvert.SerializeObject(Posts);
+                //System.IO.File.WriteAllText(@"data.txt", json);
                 return Ok();
             }
             return BadRequest();
