d935fd7d5e52deab0cc484793adee4b70bc187de
Vladlen <ilusenkovladlen@gmail.com>
2022-10-22 12:07:58 +0300

    fix: changed database/config.js

--- services/users/database/config.js
+++ services/users/database/config.js
@@ -1,7 +1,7 @@
 export default {
-    database: process.env.DB,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    host: 'localhost',
+    host: process.env.DB_HOST,
+    database: 'postgres',
     dialect: 'postgres'
 };
