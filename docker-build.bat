5dacc9c5df5afdcbb0d102f0af0337f44555a9a2
Vladlen <ilusenkovladlen@gmail.com>
2022-10-23 14:25:46 +0300

    fix: fixed problems in .bat

--- docker-build.bat
+++ docker-build.bat
@@ -1,14 +1,14 @@
 :: I'm too tired of building images manually so I've created this .bat
 :: Minikube must be started
 
-minikube -p minikube docker-env | Invoke-Expression
+:: minikube -p minikube docker-env | Invoke-Expression
 set rootDir=%cd%
 
 cd %rootDir%\services\users\
 docker build -t users:0.1 .
 
 cd %rootDir%\services\posts\
-docker build -t posts:0.1 .
+docker build -t posts:0.1 -f \PostService\DockerFile .
 
 cd %rootDir%\services\comments\
 docker build -t comments:0.1 .
@@ -17,6 +17,6 @@ cd %rootDir%\services\users\migrations\
 docker build -t users-service-migrations:0.1 .
 
 cd %rootDir%\client\
-docker build -t cleint:0.1 .
+docker build -t client:0.1 .
 
 :: Add your migrations here after you create them 
