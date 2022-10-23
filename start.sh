0eb63bded01a917004d63cc787e4eda0657a4456
Vladlen <ilusenkovladlen@gmail.com>
2022-10-23 18:20:14 +0300

    fix: fixed script

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@ minikube addons enable ingress
 
 kubectl apply -f k8s/users-postgres
 kubectl apply -f k8s/feed-postgres
-kubectl apply -f k8s/users-service
-kubectl apply -f k8s/posts-service
-kubectl apply -f k8s/comments-service
+kubectl apply -f k8s/users
+kubectl apply -f k8s/posts
+kubectl apply -f k8s/comments
 kubectl apply -f k8s/client
 
 minikube tunnel
