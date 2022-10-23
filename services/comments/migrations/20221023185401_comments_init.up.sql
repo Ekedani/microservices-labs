1d0cb8acb2acf804ba0fe6077447b5cd69682c51
Vladlen <ilusenkovladlen@gmail.com>
2022-10-23 18:58:27 +0300

    fix: fixed migrations for comments

--- services/comments/migrations/20221023185401_comments_init.up.sql
+++ services/comments/migrations/20221023185401_comments_init.up.sql
@@ -4,4 +4,7 @@ CREATE TABLE IF NOT EXISTS comments
     body VARCHAR (512) UNIQUE NOT NULL,
     post_id INT NOT NULL,
     author_id UUID NOT NULL
+    CONSTRAINT fk_posts
+        FOREIGN KEY(posts_id) 
+            REFERENCES posts(id)
 );
