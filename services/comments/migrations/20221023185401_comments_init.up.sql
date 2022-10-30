335043056dd4f8a02de051e16ff1a84bfc3806c8
Oleksii Honchar <alekseyhonchar@gmail.com>
2022-10-30 12:14:33 +0000

    fix: fixed migration

--- services/comments/migrations/20221023185401_comments_init.up.sql
+++ services/comments/migrations/20221023185401_comments_init.up.sql
@@ -1,22 +1,9 @@
-CREATE TABLE IF NOT EXISTS posts
-(
-    id INT PRIMARY KEY,
-    header VARCHAR (256) NOT NULL,
-    body VARCHAR NOT NULL,
-    post_id INT NOT NULL,
-    author_id UUID NOT NULL
-    CONSTRAINT fk_users
-        FOREIGN KEY(users_id) 
-            REFERENCES users(id)
-);
-
 CREATE TABLE IF NOT EXISTS comments
 (
     id VARCHAR (512) PRIMARY KEY,
-    body VARCHAR (512) NOT NULL,
+    body VARCHAR (512) UNIQUE NOT NULL,
     post_id INT NOT NULL,
-    author_id UUID NOT NULL
-    CONSTRAINT fk_posts
-        FOREIGN KEY(posts_id) 
-            REFERENCES posts(id)
+    author_id UUID NOT NULL,
+    -- CONSTRAINT fk_posts
+    -- FOREIGN KEY(post_id) REFERENCES posts(id)
 );
