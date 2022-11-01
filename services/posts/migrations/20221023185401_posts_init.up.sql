b501b958ba1175a0a2b98814be5253e81f8739ea
Vladlen <ilusenkovladlen@gmail.com>
2022-11-02 00:20:04 +0200

    fix: fixed migrations for posts-db

--- services/posts/migrations/20221023185401_posts_init.up.sql
+++ services/posts/migrations/20221023185401_posts_init.up.sql
@@ -1,11 +1,15 @@
-CREATE TABLE IF NOT EXISTS Posts
+/* TODO: You probably should use composite primary key IMHO;
+   TODO: You must specify that author_id is UUID for integration with users services
+   TODO: Maybe you should use a more suitable type for body?*/
+CREATE TABLE IF NOT EXISTS posts
 (
-    id int PRIMARY KEY,
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     header VARCHAR(256) NOT NULL,
     body VARCHAR(1024) NOT NULL,
     author_id VARCHAR(256) NOT NULL
 );
 
+/* TODO: You must use composite primary key IMHO */
 CREATE TABLE IF NOT EXISTS comments
 (
     id VARCHAR (512) PRIMARY KEY,
@@ -13,5 +17,5 @@ CREATE TABLE IF NOT EXISTS comments
     post_id INT NOT NULL,
     author_id UUID NOT NULL,
     CONSTRAINT fk_posts
-    FOREIGN KEY(post_id) REFERENCES Posts(id)
+    FOREIGN KEY(post_id) REFERENCES posts(id)
 );
