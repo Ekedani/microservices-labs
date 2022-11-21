e7cdb07bb2b3d795b75f4426233ff38f581f7119
Vladlen <ilusenkovladlen@gmail.com>
2022-11-21 18:59:05 +0200

    feat: implemented sync communication between comments and users

--- services/comments/src/controllers/Comment.Controller.js
+++ services/comments/src/controllers/Comment.Controller.js
@@ -5,8 +5,17 @@ const CommentController = {
     async getAllComments(req, res, next) {
         try {
             const { post_id } = req.params;
-            const result = await Comment.findAll({where: { post_id }});
-            res.send({ comments: result });
+            const comments = await Comment.findAll({where: { post_id }});
+            const authorIds = new Set(comments.map(x => x.author_id));
+            const authors = await new Promise.all([...authorIds].map(id => fetch(`http://users-service/api/users/${id}`)));
+            comments.forEach(comment => {
+                const author = authors.find(x => x.ok && x.body.author_id === comment.author_id);
+                if(author){
+                    comment.author_username = author.body.username;
+                    comment.author_tag = author.body.author_tag;
+                }
+            });
+            res.send({ comments });
         } catch (err) {
             next(err);
         }
@@ -19,6 +28,11 @@ const CommentController = {
             if (!result) {
                 throw createError(404, 'This comment doesn`t exist');
             }
+            const author = await fetch(`http://users-service/api/users/${result.author_id}`);
+            if (author.ok) {
+                result.author_username = author.body.username;
+                result.author_tag = author.body.tag;
+            }
             res.send(result);
         } catch (err) {
             next(err);
@@ -28,11 +42,12 @@ const CommentController = {
     async addComment(req, res, next) {
         try {
             const comment_id = `${Date.now()}_${req.params.post_id}_${req.body.author_id}`;
+            const { post_id } = req.params;
             const comment = Comment.build({
                 id: comment_id,
                 body: req.body.body,
                 author_id: req.body.author_id,
-                post_id: req.body.post_id
+                post_id
             });
             const result = await comment.save();
             res.send(result);
