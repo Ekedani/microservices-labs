6db49b898c96fa03597614a2cf60ea7e109b22a7
Vladlen <ilusenkovladlen@gmail.com>
2022-11-21 19:11:01 +0200

    fix: fixed problems in comments service

--- services/comments/src/controllers/Comment.Controller.js
+++ services/comments/src/controllers/Comment.Controller.js
@@ -1,5 +1,6 @@
 import Comment from '../models/Comment.Model.js';
 import createError from 'http-errors';
+import fetch from 'node-fetch';
 
 const CommentController = {
     async getAllComments(req, res, next) {
@@ -7,7 +8,7 @@ const CommentController = {
             const { post_id } = req.params;
             const comments = await Comment.findAll({where: { post_id }});
             const authorIds = new Set(comments.map(x => x.author_id));
-            const authors = await new Promise.all([...authorIds].map(id => fetch(`http://users-service/api/users/${id}`)));
+            const authors = await Promise.all([...authorIds].map(id => fetch(`http://users-service/api/users/${id}`)));
             comments.forEach(comment => {
                 const author = authors.find(x => x.ok && x.body.author_id === comment.author_id);
                 if(author){
